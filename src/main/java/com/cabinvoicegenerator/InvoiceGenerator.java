20031bc545b40b8c10fb340d09665843b647d3cd
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-25 21:07:23 +0530

    Welcome

---
+++ src/main/java/com/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome cab invoice generator");
+    }
+}
